f737f690aaa0cabce99b41041558af2ba03cf4e7
saravp123 <39334476+saravp123@users.noreply.github.com>
2019-04-30 11:59:55 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,11 +8,12 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1syS4sV4ZOEQThWNEHn8AUNebJ7Ez1hjP'
+export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+#export_file_url = 'https://drive.google.com/uc?export=download&id=1syS4sV4ZOEQThWNEHn8AUNebJ7Ez1hjP'
 export_file_name = 'export.pkl'
 
-classes = ['desk','printer','chair','recliner']
+#classes = ['desk','printer','chair','recliner']
+classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
 app = Starlette()
